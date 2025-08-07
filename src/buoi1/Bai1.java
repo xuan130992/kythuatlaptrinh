772c4edca9d04ab20ebff590323fee0ffca4ce02
xuan.le <thanhxuan130992@gmail.com>
2025-08-07 19:06:48 +0700

    ngay4

---
+++ src/buoi1/Bai1.java
@@ -0,0 +1,23 @@
+package buoi1;
+
+public class Bai1 {
+//	dau vao - input:
+//		+so nguyen n
+//		
+//	dau ra -output:
+//		+True/false -return
+		static void KiemTraChinPhuong(int n) {
+			if(n%3 ==0) {
+			boolean	chinPhuong = true;
+			System.out.println("chin Phuong ko "+chinPhuong);
+				
+			}
+			boolean chinPhuong= false;
+			System.out.println("chin Phuong ko "+chinPhuong);
+		}
+
+	public static void main(String[] args) {
+		KiemTraChinPhuong(4);
+	}
+
+}
--- src/buoi1/BienToanCuc.java
+++ src/buoi1/BienToanCuc.java
@@ -4,12 +4,16 @@ public class BienToanCuc {
 	static int x=36;
 	
 	static void funcA() {
-		int x=369;
+		 int x=369;
+		System.out.println("gia tri x"+BienToanCuc.x);
 	}
 	
 
 	public static void main(String[] args) {
-		System.out.println("gia tri x la ");
+		
+	
+		System.out.println("gia tri x la "+x);
+		funcA();
 		
 
 	}
